744e4e35c20193694c3bd4ff2a857eeca145a254
Drawserqzez <me@holton.lol>
2023-03-17 23:55:12 +0100

    Add better info, save to $POMO_DATA file

--- functions/__pomo.track.fish
+++ functions/__pomo.track.fish
@@ -1,3 +1,9 @@
 function __pomo.track -d "Tracks how long you've worked"
-    echo "Great job, you worked since $POMO_STARTED!"
+    set -f now (date +%s)
+    set -f diff (math (math $now - $POMO_STARTED) / 60)
+    set -f day (date +%D)
+
+    echo "$day - $diff seconds" >> $POMO_DATA 
+
+    echo "Great job, you worked for approx. $diff seconds!"
 end
